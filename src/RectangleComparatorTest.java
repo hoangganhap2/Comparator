c6defb79b953290cc17cdab29d9df0c8daf9b018
Hoang <cct.cry@gmai.com>
2019-11-01 17:11:08 +0700

    p

---
+++ src/RectangleComparatorTest.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class RectangleComparatorTest {
+    public static void main(String[] args) {
+        Rectangle []rectangles = new Rectangle[3];
+        rectangles[0] = new Rectangle(3,4);
+        rectangles[1] = new Rectangle(2,4,"red",false);
+        rectangles[2] = new Rectangle();
+
+        System.out.println("Pre - sorted");
+        for (Rectangle rectangle : rectangles) {
+            System.out.println(rectangle);
+        }
+        Comparator rectangleComparator = new RectangleComparator();
+        Arrays.sort(rectangles,rectangleComparator);
+        System.out.println("After - sorted");
+        for(Rectangle rectangle : rectangles){
+            System.out.println(rectangle);
+        }
+    }
+}
